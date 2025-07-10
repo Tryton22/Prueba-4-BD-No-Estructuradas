fad0e73234018ba3e32b715d586fe43019a2677d
Matías Fonseca <89324400+Tryton22@users.noreply.github.com>
2025-07-10 12:42:59 -0400

    Create conexion.py

---
+++ mysql_consultas/conexion.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+def con():
+    try:
+        mydb =mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="",
+        database="actividad2"
+    )
+
+        if mydb.is_connected():
+            print("conexion exitosa!!")
+        return mydb
+
+    except Error as e:
+        print("Error al conectar!")
+        return None
+
+mydb= con()
